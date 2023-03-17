503555bf9344760c9f18cb634f4bc8b59cca605e
Mokhamad Susmana <susmanam@gmail.com>
2023-03-17 15:08:12 +0700

    edit url api for images detail product

--- src/pages/DetailProduct/index.js
+++ src/pages/DetailProduct/index.js
@@ -72,7 +72,7 @@ const [size, setSize] = useState(0);
             {dataImages.map((item)=> {
               if (item.id === id) {
                 return(                                
-                  <img src={`http://localhost:5000/uploads/images/${item.images[0].filename}`} className="card-img-top rounded-circle w-75 mx-auto" style={{height: '55vh'}} alt="..." ></img>
+                  <img src={`https://coffeeshopbe.cyclic.app/uploads/images/${item.images[0].filename}`} className="card-img-top rounded-circle w-75 mx-auto" style={{height: '55vh'}} alt="..." ></img>
                 )
               }})}    
 
@@ -130,7 +130,7 @@ const [size, setSize] = useState(0);
             {dataImages.map((item)=> {
               if (item.id === id) {
                 return(                 
-                    <img src={`http://localhost:5000/uploads/images/${item.images[0].filename}`} alt="" className="rounded-circle" style={{width: '25vw', height:'18vh'}} />
+                    <img src={`https://coffeeshopbe.cyclic.app/uploads/images/${item.images[0].filename}`} alt="" className="rounded-circle" style={{width: '25vw', height:'18vh'}} />
                 )
               }
             })} 
